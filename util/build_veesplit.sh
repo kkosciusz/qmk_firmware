b828cdb4dd1c2a045421898ce895716394312d22
kkosciusz <k.kosciuszkiewicz@gmail.com>
2023-11-28 20:25:41 +0100

    add shell command to build and copy veesplit56 fw

---
+++ util/build_veesplit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+util/docker_build.sh handwired/veesplit56:default
+target_dir=/mnt/c/Users/kosciusz/Documents/Private/klawiatura\ veesplit56/firmware
+file_name=$(echo *.uf2 | head -1)
+modified=$(date --date=@$(stat -c '%Y' ${file_name}) +%Y-%m-%d)
+target_file=${file_name%.uf2}-${modified}.uf2
+echo COPY "$file_name" '->' "$target_dir/$target_file"
+cp "$file_name" "$target_dir/$target_file"
